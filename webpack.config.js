529613cd5124a91ac6ce8bf8f6801a81499b661d
stekolschikov <stekolschikov@mail.ua>
2017-10-02 17:01:44 +0300

    add loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,11 +30,28 @@ var config = {
                 })
             },
             {
-                test: /\.css/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
             }
         ]
     },
